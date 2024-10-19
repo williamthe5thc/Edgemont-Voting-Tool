708925dfddcc53eac2503038b4daf6106722068a
W. Jordan Charles <williamthe5thc@icloud.com>
2024-10-19 11:02:05 -0600

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,15 @@
-// constants.js
+/**
+ * constants.js
+ * 
+ * This file defines constant values used throughout the application.
+ * It includes:
+ * 
+ * 1. CATEGORIES: An array of competition categories
+ * 2. THEME: The theme of the cooking competition
+ * 
+ * Centralizing these constants ensures consistency across the application
+ * and makes it easier to update competition details in the future.
+ */
 
 export const CATEGORIES = [
     'Bread',
