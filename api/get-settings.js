0f4609a6d63640ad9c2a2b23836d409c7f1ade39
W. Jordan Charles <williamthe5thc@icloud.com>
2024-10-20 17:33:52 -0600

    Update get-settings.js

--- api/get-settings.js
+++ api/get-settings.js
@@ -7,18 +7,11 @@
  */
 
 import { getKVData, setKVData, handleApiError, methodNotAllowed } from './utils';
-
+import {CATEGORIES} from '/js/constants.js'
 // Default values for dish counts
 const DEFAULT_MIN_DISH_COUNT = 1;
 const DEFAULT_MAX_DISH_COUNT = 5;
 
-// List of competition categories
-const CATEGORIES = [
-    'Bread',
-    'Appetizers',
-    'Dessert',
-    'Entrée'
-];
 
 /**
  * API handler for retrieving competition settings
