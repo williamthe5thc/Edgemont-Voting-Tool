341e0672001c774f0c31735952ea82ecc9f0a8b5
W. Jordan Charles <williamthe5thc@icloud.com>
2024-10-21 00:34:41 -0600

    Update categoryLoader.js

--- js/categoryLoader.js
+++ js/categoryLoader.js
@@ -102,7 +102,7 @@ export async function loadCategoriesProgressively() {
             const categoryDiv = document.createElement('div');
             categoryDiv.classList.add('category');
             categoryDiv.innerHTML = `
-                <h2>${category} <sup>*</sup> <small>(optional)</small>  </h2>
+                <h2>${category} <small><sup>*(optional)</small></sup>  </h2>
                 <p>Type your favorite dishes (up to 2):</p>
                 ${Array(2).fill().map(() => `
                     <input type="text" 
