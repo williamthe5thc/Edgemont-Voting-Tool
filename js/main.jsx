f20f028601569d21fc39fdd0d79289541b9d8c24
W. Jordan Charles <williamthe5thc@icloud.com>
2024-10-22 15:11:02 -0600

    Update main.jsx

--- js/main.jsx
+++ js/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Preloader from './components/Preloader';
-import App from './App';
+import App from './components/App.jsx';  // Updated import path
 import '../styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
